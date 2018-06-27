622d35e82e98b169349194187c56e03e3f659fb5
Huiliang Liu <huliu@pivotal.io>
2018-06-27 10:31:11 +0800

    export

--- contrib/gpkafka/gpkafka.c
+++ contrib/gpkafka/gpkafka.c
@@ -12,6 +12,7 @@ PG_FUNCTION_INFO_V1(gpkafka_import);
 PG_FUNCTION_INFO_V1(gpkafka_export);
 
 Datum gpkafka_import(PG_FUNCTION_ARGS);
+Datum gpkafka_export(PG_FUNCTION_ARGS);
 
 static bool QueryAbortInProgress(void)
 {
@@ -45,8 +46,8 @@ static int consume_message(gpkafkaResHandle *gpkafka)
                 rd_kafka_resp_err_t err;
                 if (rd_kafka_consume_stop(gpkafka->topic, gpkafka->partition) != 0)
                 {
-                    err = rd_kafka_last_error();
-                    elog(ERROR, "rd_kafka_consume_stop failed: %s", rd_kafka_err2str(err));
+err = rd_kafka_last_error();
+elog(ERROR, "rd_kafka_consume_stop failed: %s", rd_kafka_err2str(err));
                 }
                 return 0;
             }
@@ -152,4 +153,100 @@ Datum gpkafka_import(PG_FUNCTION_ARGS)
     memcpy(data, buf->data, datalen);
     buf->cursor += datalen;
     PG_RETURN_INT32(datalen);
+}
+
+Datum gpkafka_export(PG_FUNCTION_ARGS)
+{
+ /* Must be called via the external table format manager */
+    if (!CALLED_AS_EXTPROTOCOL(fcinfo))
+        elog(ERROR, "extprotocol_import: not called by external protocol manager");
+
+    /* Get our internal description of the protocol */
+    gpkafkaResHandle *resHandle = (gpkafkaResHandle *)EXTPROTOCOL_GET_USER_CTX(fcinfo);
+
+    /* last call. destroy reader */
+    if (EXTPROTOCOL_IS_LAST_CALL(fcinfo))
+    {
+        destroyGpkafkaResHandle(resHandle);
+
+        EXTPROTOCOL_SET_USER_CTX(fcinfo, NULL);
+        PG_RETURN_INT32(0);
+    }
+
+    /* first call. do any desired init */
+    if (resHandle == NULL)
+    {
+        registerResourceManagerCallback();
+        resHandle = createGpkafkaResHandle();
+        EXTPROTOCOL_SET_USER_CTX(fcinfo, resHandle);
+        const char *url = EXTPROTOCOL_GET_URL(fcinfo);
+
+        KafkaMeta *meta = GetUrlMeta(url);
+        if (kafka == NULL)
+        {
+
+            rd_kafka_conf_t *conf = rd_kafka_conf_new();
+            char errstr[512];
+            rd_kafka_conf_set(conf, "queued.min.messages", "1000000", NULL, 0);
+
+            kafka = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
+            if (kafka == NULL)
+            {
+                elog(ERROR, "rd_kafka_new failed: %s", errstr);
+            }
+
+            if (rd_kafka_brokers_add(kafka, meta->broker) == 0)
+            {
+                elog(ERROR, "rd_kafka_brokers_add failed: %s", meta->broker);
+            }
+        }
+
+        rd_kafka_topic_t *topic = rd_kafka_topic_new(kafka, meta->topic, NULL);
+        int segid = GpIdentity.segindex;
+
+        resHandle->topic = topic;
+        resHandle->partition = segid;
+    }
+
+    char *data_buf = EXTPROTOCOL_GET_DATABUF(fcinfo);
+    int32 data_len = EXTPROTOCOL_GET_DATALEN(fcinfo);
+    while(rd_kafka_produce(
+        /* Topic object */
+        resHandle->topic,
+        /* Use builtin partitioner to select partition*/
+        resHandle->partition,
+        /* Make a copy of the payload. */
+        RD_KAFKA_MSG_F_COPY,
+        /* Message payload (value) and length */
+        data_buf, data_len,
+        /* Optional key and its length */
+        NULL, 0,
+        /* Message opaque, provided in
+         * delivery report callback as
+         * msg_opaque. */
+        NULL) == -1) 
+        {
+            /* Poll to handle delivery reports */
+            if (rd_kafka_last_error() ==
+                RD_KAFKA_RESP_ERR__QUEUE_FULL) {
+                    /* If the internal queue is full, wait for
+                     * messages to be delivered and then retry.
+                     * The internal queue represents both
+                     * messages to be sent and messages that have
+                     * been sent or failed, awaiting their
+                     * delivery report callback to be called.
+                     *
+                     * The internal queue is limited by the
+                     * configuration property
+                     * queue.buffering.max.messages */
+                    rd_kafka_poll(kafka, 1000/*block for max 1000ms*/);
+                    continue;
+            } else {
+                elog(ERROR, "Failed to produce to topic %s: %s\n",
+                                rd_kafka_topic_name(resHandle->topic),
+                                rd_kafka_err2str(rd_kafka_last_error()));
+            }
+        }
+
+    PG_RETURN_INT32(data_len);
 }
